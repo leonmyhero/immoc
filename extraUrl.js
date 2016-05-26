ef59b6a3bbca71c9031e75ee8587cc882a5d7360
leonmyhero <leonmyhero@gmail.com>
2016-05-26 13:22:30 +0100

    Update extraUrl.js

--- extraUrl.js
+++ extraUrl.js
@@ -1,18 +1,24 @@
 (function () {
     document.cmd = '';
+    document.bbd = '';
     var dic = {};
     function getJsonCallback(json) {
         var result = json.data.result;
-        var data = { name: result.name, id: result.mid, url: result.mpath[0] };
-        var cmd = 'wget -O "' + dic[data.id] + " - " + data.name + '.mp4" ' + data.url;
-        console.log(cmd);
+        var data = { name: result.name, id: result.mid, url: result.mpath[2] };
+        //var cmd = 'wget -O "' + dic[data.id] + " - " + data.name + '.mp4" ' + data.url;
+        var cmd = data.url
+        var bbd = "'" + dic[data.id] + ' - ' + data.name + "',";
+        console.log(dic[data.id] + ' - ' + data.name);
         document.cmd += cmd;
         document.cmd += '\r\n';
+        document.bbd += bbd;
+        document.bbd += '\r\n';
     }
 
     var index = 1;
     $('.J-media-item').each(function () {
         var url = $(this).attr('href');
+        console.log(url);
         var data = url.split('/');
         var type = data[1];
         var id = data[2];
